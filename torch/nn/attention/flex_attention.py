8fc6f42901062e283dec8e4c55eb29b6247a18f8
Yanbo Liang <ybliang8@gmail.com>
2024-07-23 15:49:27 -0700

    Update docs

--- torch/nn/attention/flex_attention.py
+++ torch/nn/attention/flex_attention.py
@@ -29,6 +29,7 @@ __all__ = [
     "create_mask",
     "or_masks",
     "and_masks",
+    "noop_mask",
 ]
 
 _score_mod_signature = Callable[[Tensor, Tensor, Tensor, Tensor, Tensor], Tensor]
@@ -122,6 +123,7 @@ def noop_mask(
     token_q: Tensor,
     token_kv: Tensor,
 ) -> Tensor:
+    """Returns a noop mask_mod"""
     return batch.new_ones(size=(), dtype=torch.bool, device=batch.device)
 
 
